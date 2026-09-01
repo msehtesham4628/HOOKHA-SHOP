0ac270c05dd3e7f7d49e5552c705387481715828
launchnowstudio <studiolaunchnow@gmail.com>
2026-09-01 22:10:59 +0530

    chore: update next.config.js for Next 15 compatibility (remove swcMinify warning, guard splitChunks)

--- next.config.js
+++ next.config.js
@@ -1,35 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.amazonaws.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.example.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'blob.vercel.com',
-      },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: '*.amazonaws.com' },
+      { protocol: 'https', hostname: 'cdn.example.com' },
+      { protocol: 'https', hostname: 'blob.vercel.com' },
     ],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   webpack: (config, { isServer }) => {
-    if (!isServer) {
+    if (!isServer && config.optimization && config.optimization.splitChunks) {
       config.optimization.splitChunks.cacheGroups = {
         ...config.optimization.splitChunks.cacheGroups,
         three: {
@@ -40,7 +27,7 @@ const nextConfig = {
           enforce: true,
         },
         fiber: {
-          test: /[\\/]node_modules[\\/](@react-three)[\\/]/,
+          test: /[\\/]node_modules[\\/](?:@react-three)[\\/]/,
           name: 'fiber',
           priority: 31,
           reuseExistingChunk: true,
